c513f365bfc3883691649f3f60c15c1f584c7250
Buddy Chambers <blueshift@intelligent-d2.com>
2020-06-14 13:53:23 -0500

    Removed old frontend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -7,6 +7,18 @@ class App extends React.Component {
             <div className="container">
                 <div id="task-container">
                     <div id="form-wrapper">
+                         <form id="form">
+                            <div className="flex-wrapper">
+                                <div style={{flex: 6}}>
+                                    <input className="form-control" id="title" type="text" name="title" placeholder="Add task.." />
+                                </div>
+
+                                 <div style={{flex: 1}}>
+                                    <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                                 </div>
+                            </div>
+                        </div>
+
                     </div>
 
                     <div id="list-wrapper">
