326dda3327e32237610a47a920d7e2152635b49b
Buddy Chambers <blueshift@intelligent-d2.com>
2020-06-16 01:00:45 -0500

    Working on task app, and added Task to admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,28 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            todoList: [],
+            activeItem: {
+                id: null,
+                title: '',
+                completed: false,
+            },
+            editing: false,
+        }
+        // this.fetchTasks = this.fetchTasks.bind(this)
+    };
+
+    componentDidMount() {
+        this.fetchTasks()
+    }
+
+    fetchTasks() {
+        console.log('Fetching...')
+    }
+
     render() {
         return(
             <div className="container">
